e07e244baefbf92fa81ee0a3f01d6a1cf77a9677
Akira Sawada <aklaswad@gmail.com>
2023-09-29 21:55:29 +0900

    Add margin

--- extra/py2max/all-classes.py
+++ extra/py2max/all-classes.py
@@ -7,10 +7,10 @@ y=20
 for k,v in MAXCLASS_DEFAULTS.items():
     com_rect = Rect(x=20.0, y=y, w=200.0, h=18.0)
     p.add_comment(text=k, patching_rect=com_rect)
-    y += 20
+    y += 30
     def_rect=v["patching_rect"]
     obj_rect = Rect(x=20.0,y=y,w=def_rect.w, h=def_rect.h)
     p.add(k, patching_rect=obj_rect)
-    y += obj_rect.h + 10.0
+    y += obj_rect.h + 15.0
 
 p.save()
